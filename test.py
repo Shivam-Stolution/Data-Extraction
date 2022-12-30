d72cff9ffc1b10e1886a56baabb221dd1b702202
Shivam <shvm.pratap@gmail.com>
2022-12-30 22:45:57 +0530

    A comment is added to the file, test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import requets
+import requests
 from bs4 import BeautifulSoup
 url = "Google.com"
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text,"html.parser")
+
+# soup =  BeautifulSoup(resposne.text,"lxml")
